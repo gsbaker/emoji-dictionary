12ca49d09f9015be6a8b14fa1bee7b6cd24dcc12
gsbaker <george@gsbaker.uk>
2020-05-28 13:45:52 +0100

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by George Baker on 28/05/2020.
+//  Copyright © 2020 George Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
