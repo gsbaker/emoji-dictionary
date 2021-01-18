3d4c93c6a068969d86f8edc05fb771a6de72bd21
gsbaker <gsbaker2000@icloud.com>
2021-01-18 10:39:35 +0000

    Added row deletion

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -108,18 +108,16 @@ class EmojiTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            emojis.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
 
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
@@ -131,7 +129,7 @@ class EmojiTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) ->
     UITableViewCell.EditingStyle {
-        return .none
+        return .delete
     }
 
     /*
